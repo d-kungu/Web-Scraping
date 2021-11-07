ed84cca757b89cc0e841cd059de1269073109ad2
Diana Kung'u <dkungu@gmail.com>
2021-11-07 17:04:29 +0300

    New Script

---
+++ Worldbank_API.py
@@ -0,0 +1,57 @@
+
+'''Collecting data from World Bank API 
+- Proportion number of people living in urban areas SP.URB.TOTL.IN.ZS 2010 - 2020
+- https://datahelpdesk.worldbank.org/knowledgebase/topics/125589
+
+    ** Created by Diana Kung'u **
+
+'''
+import requests
+import pandas as pd
+import json
+
+
+
+#1. Connect to the API
+url = "http://api.worldbank.org/V2/country/all/indicator/SP.URB.TOTL.IN.ZS?format=json&date=2010:2020"
+request = requests.get(url)
+print(request.status_code) # 200 Status indicates successful connection
+
+#2 Pull the data from the connection
+data = request.text
+
+#3 Parse data into JSON format & get number of pages
+data =json.loads(data)
+pages = data[0].get('pages')
+
+
+
+pg = 1 #Initialize page numbers
+results = []
+
+
+#Loop through all pages and extract data
+while pg <= pages:
+    url = f"http://api.worldbank.org/V2/country/all/indicator/SP.URB.TOTL.IN.ZS?format=json&date=2010:2020&page={pg}"
+    request = requests.get(url)
+    data = json.loads(request.text)
+    results.extend(data[1])
+    pg += 1
+
+
+#Parse list of nested dictionaries to create a dataframe
+df =pd.json_normalize(results) 
+
+
+#Reshape dataframe to have each country on one row and years on columns
+df.drop(['indicator.value', 'indicator.id', 'decimal', 'obs_status', 'unit'], axis= 1, inplace= True)
+df = df.pivot(index=['countryiso3code','country.id', 'country.value'], columns='date').reset_index()
+
+
+df.columns = [' '.join(col).strip() for col in df.columns.values]
+
+#OUTPUT DATA
+df.to_csv(r'.\Data\WB_urban_population.csv', index = False)
+print(df.head())
+print('End')
+
--- books_scraper_with_bs4.py
+++ books_scraper_with_bs4.py
@@ -1,3 +1,10 @@
+''' Web Scrapper for collecting data from e-commerce like webpage
+ https://books.toscrape.com/ a dummy website for practicing web crawling.
+
+Written by: Diana Kung'u
+
+'''
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as bs
@@ -5,19 +12,20 @@ from bs4 import BeautifulSoup as bs
 all_books = []
 
 
+# Function that  loads page and return all listed books details
 def page(url):
     r = requests.get(url)
     soup = bs(r.text, "lxml")
     return soup.find_all("article", attrs={"class": "product_pod"})
 
 
+#Function that extract required information from each listing
 def extract(listings):
     for listing in listings:
         base_link = "http://books.toscrape.com/catalogue/"
         link = listing.find("a").get("href")
-        book_link = base_link + link
-        # print(link)
-
+        book_link = base_link + link # create product list link
+       
         # get the link of each product
         book_page = requests.get(book_link)
         soup_2 = bs(book_page.text, "lxml")
@@ -34,16 +42,16 @@ def extract(listings):
 
         book = {"title": title, "price": price, "upc": upc}
         all_books.append(book)
-    # return
+    
 
 
 pg = 1 # initialize page numbers
 
-while True:
+while True: # Loop through all pages on the site
     url = f"http://books.toscrape.com/catalogue/page-{pg}.html"
     print(f"extracting page {pg}")
     listings = page(url)
-    if len(listings) == 0:
+    if len(listings) == 0: #Terminate if page has no product listings
 
         break
     else:
